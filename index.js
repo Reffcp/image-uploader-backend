88d15ded29cf23d2f1d84ea95ce3cc0d7ad385b6
reffcp-ultratech <crefugio-ext@ultratech-inc.com>
2024-06-06 02:39:20 -0600

    refactor: :fire: se agrega https y certificados de prueba para localhost, para que pod tenga certificados validos

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const errors = require("./src/network/errors");
+const router = require("./src/network/routes");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+router(app);
+app.use(errors);
+app.use(express.urlencoded({ extended: false }));
+//obtener la ruta base donde se encuentra el proyecto
+const pathBase = __dirname;
+const options = {
+  key: fs.readFileSync(pathBase + process.env.WEB_SERVER_PRIVATE_KEY_PATH),
+  cert: fs.readFileSync(pathBase + process.env.WEB_SERVER_FULLCHAIN_CERT_PATH),
+};
+
+https
+  .createServer(options, app)
+  .listen(process.env.API_PORT, process.env.API_HOST, () => {
+    console.log(
+      `Server is running on https://${process.env.API_HOST}:${process.env.API_PORT}`
+    );
+  });
+
+module.exports = app;
