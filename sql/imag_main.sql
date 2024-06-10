0ced1dedb72af30879464316b8909111e978d9b5
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-06-10 08:27:09 -0600

    feat: :sparkles: se crea un evento en la DB para retirar los registros caducados

--- sql/imag_main.sql
+++ sql/imag_main.sql
@@ -1,3 +1,9 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 10-06-2024 a las 14:23:43
 -- Versión del servidor: 10.11.7-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -14,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `imag_main`
 --
+CREATE DATABASE IF NOT EXISTS `imag_main` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `imag_main`;
 
 -- --------------------------------------------------------
 
@@ -21,15 +29,16 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `imagenes`
 --
 
-CREATE TABLE `imagenes` (
-  `imagen_id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `imagenes` (
+  `imagen_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `imagen_url` varchar(255) NOT NULL,
   `imagen_meta_s3` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`imagen_meta_s3`)),
   `fecha_hora_creacion` datetime DEFAULT current_timestamp(),
   `fecha_hora_borrar` datetime DEFAULT NULL,
   `imagen_tamanio` double DEFAULT 0,
   `ip_origen` varchar(15) DEFAULT '0.0.0.0',
-  `registro_activo` tinyint(1) DEFAULT 1
+  `registro_activo` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`imagen_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -44,17 +53,17 @@ END
 $$
 DELIMITER ;
 
+DELIMITER $$
 --
--- Indices de la tabla `imagenes`
+-- Eventos
 --
-ALTER TABLE `imagenes`
-  ADD PRIMARY KEY (`imagen_id`);
+CREATE DEFINER=`root`@`localhost` EVENT `desactivar_registros_vencidos` ON SCHEDULE EVERY 1 DAY STARTS '2024-06-10 14:21:33' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+  UPDATE imagenes
+  SET registro_activo = 0
+  WHERE fecha_hora_borrar <= NOW() AND fecha_hora_borrar IS NOT NULL;
+END$$
 
---
--- AUTO_INCREMENT de la tabla `imagenes`
---
-ALTER TABLE `imagenes`
-  MODIFY `imagen_id` bigint(20) NOT NULL AUTO_INCREMENT;
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
